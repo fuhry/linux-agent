2eba5e971014c9f72c64a129e6ca386b5e4714c5
Nick Garvey <ngarvey@dattobackup.com>
2013-06-07 16:02:28 -0400

    Add initial device mapper code. Currently can only duplicate.

---
+++ DeviceMapper/devicemapper.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python
+
+__author__ = 'ngarvey'
+
+from ctypes import *
+from ctypes.util import *
+import dmconstants
+import sys
+
+# libdevmapper setup
+_libdevmapper_path = find_library('devmapper')
+_libdevmapper = CDLL(_libdevmapper_path, use_errno=True)
+
+_libdevmapper.dm_log_init_verbose.restype = None
+_libdevmapper.dm_log_init_verbose(10)
+
+
+# Dummy class to help with type checking
+class _DmTask(Structure):
+    pass
+
+
+class DmTarget(object):
+    start = None
+    size = None
+    target_type = None
+    params = None
+
+    def __init__(self, start=None, size=None, target_type=None, params=None):
+        self.start = start
+        self.size = size
+        self.target_type = target_type
+        self.params = params
+
+
+## This section initializes the parameters and return values for all of the
+## _libdevmapper functions. This is used by ctypes to do type checking and
+## conversion.
+
+# dm_task_create
+_libdevmapper.dm_task_create.restype = POINTER(_DmTask)
+
+# dm_task_set_uuid
+_libdevmapper.dm_task_set_uuid.argtypes = [POINTER(_DmTask), c_char_p]
+
+# dm_task_run
+_libdevmapper.dm_task_run.argtypes = [POINTER(_DmTask)]
+
+# dm_get_next_target
+_libdevmapper.dm_get_next_target.argtypes = [POINTER(_DmTask), c_void_p, POINTER(c_uint64),
+                                            POINTER(c_uint64), POINTER(c_char_p), POINTER(c_char_p)]
+_libdevmapper.dm_get_next_target.restype = c_void_p
+
+# dm_task_add_target
+_libdevmapper.dm_task_add_target.argtypes = [POINTER(_DmTask), c_uint64, c_uint64, c_char_p, c_char_p]
+
+# dm_task_set_name
+_libdevmapper.dm_task_set_name.argtypes = [POINTER(_DmTask), c_char_p]
+
+
+def _get_targets(dm_table_task):
+    targets = []
+
+    next = None
+    start = c_uint64(-1)
+    size = c_uint64(-1)
+    target_type = c_char_p()
+    params = c_char_p()
+    next = _libdevmapper.dm_get_next_target(dm_table_task, c_void_p(next), byref(start), byref(size),
+                                           byref(target_type), byref(params))
+
+    targets.append(DmTarget(start.value, size.value, target_type.value, params.value))
+    while next:
+        next = _libdevmapper.dm_get_next_target(dm_table_task, c_void_p(next), byref(start), byref(size),
+                                               byref(target_type), byref(params))
+        targets.append(DmTarget(start.value, size.value, target_type.value, params.value))
+
+    return targets
+
+
+def _copy_targets(dm_table_task, dm_create_task):
+    targets = _get_targets(dm_table_task)
+    for target in targets:
+        _libdevmapper.dm_task_add_target(dm_create_task, target.start, target.size, target.target_type, target.params)
+
+
+def duplicate_table(source_name, destination_name):
+    # Create TABLE task
+    dm_table_task = _libdevmapper.dm_task_create(dmconstants.DM_DEVICE_TABLE)
+    _libdevmapper.dm_task_set_name(dm_table_task, source_name)
+
+    # Create CREATE task
+    dm_create_task = _libdevmapper.dm_task_create(dmconstants.DM_DEVICE_CREATE)
+    _libdevmapper.dm_task_set_name(dm_create_task, destination_name)
+
+    # Execute TABLE task
+    _libdevmapper.dm_task_run(dm_table_task)
+
+    # Copy targets from TABLE task to CREATE task
+    _copy_targets(dm_table_task, dm_create_task)
+
+    # Execute CREATE task
+    _libdevmapper.dm_task_run(dm_create_task)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        sys.stderr.write("usage: " + sys.argv[0] + " dm_source dm_destination\n")
+        exit(1)
+    duplicate_table(sys.argv[1], sys.argv[2])
