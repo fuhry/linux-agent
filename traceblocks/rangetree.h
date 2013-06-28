ee88b1f1b4f805852c979236db576ed08b0cff0d
Nick Garvey <ngarvey@dattobackup.com>
2013-06-28 16:00:33 -0400

    Add comment to add_value

--- traceblocks/rangetree.h
+++ traceblocks/rangetree.h
@@ -11,6 +11,14 @@ struct range {
 	long end;
 };
 
+/* Add a single value to the heap-allocated range tree.
+ *
+ * If the value is already in the tree, the range it is
+ * in is returned. Otherwise, the new range is returned.
+ *
+ * If two ranges overlap after the addition, the ranges
+ * are merged.
+ */
 extern struct range *add_value(long, struct rb_root *);
 
 #endif
