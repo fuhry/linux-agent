f19bedbdc218df3cb0352d2fac211d5323fb15d4
Nick Garvey <ngarvey@dattobackup.com>
2013-07-07 16:27:07 -0400

    Added comment specifying dependencies

--- helper_scripts/send_msg.py
+++ helper_scripts/send_msg.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 
 # Helper script to test sending messages to the datto daemon (dattod)
+#
+# Uses http://semanchuk.com/philip/posix_ipc/
+# You will need to install python-dev to install posix_ipc
 
 import posix_ipc
 
