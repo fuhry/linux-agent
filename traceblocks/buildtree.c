269d0a1b42e09f6155e79554f95443bf51572642
Nick Garvey <ngarvey@dattobackup.com>
2013-07-10 02:05:25 -0400

    Added comments and clarified code

--- traceblocks/buildtree.c
+++ traceblocks/buildtree.c
@@ -10,8 +10,9 @@ int main()
 	struct range *range = NULL;
 
 	int read_bytes = -1;
-	struct blk_io_trace buf;
+	struct blk_io_trace trace;
 	long total_read_bytes;
+	unsigned int num_sectors;
 
 	unsigned int i;
 	
@@ -21,20 +22,31 @@ int main()
 		return 1;
 	}
 
-	while ((read_bytes = fread(&buf, sizeof(struct blk_io_trace), 1,
+	/* From stdin, read in a blk_io_trace struct. stdin should be the output
+	 * from blktrace or something similar */
+	while ((read_bytes = fread(&trace, sizeof(struct blk_io_trace), 1,
 					stdin) > 0)) {
 		total_read_bytes += read_bytes;
-		if (buf.magic != (BLK_IO_TRACE_MAGIC | BLK_IO_TRACE_VERSION)) {
-			fprintf(stderr, "got magic: 0x%x\n", buf.magic);
+
+		/* Sanity check, make sure the blk_io_trace struct has the right format */
+		if (trace.magic != (BLK_IO_TRACE_MAGIC | BLK_IO_TRACE_VERSION)) {
+			fprintf(stderr, "got magic: 0x%x\n", trace.magic);
 			fprintf(stderr, "total_bytes: %ld\n",
 					total_read_bytes);
 			break;
 		}
-		for (i = 0; i < buf.bytes / 512; i++) {
-			rt_add_value(buf.sector + i, range_tree);
+
+		
+		/* Calculate the number of sectors in this trace */
+		num_sectors = trace.bytes / 512;
+
+		/* Add each sector to the tree */
+		for (i = 0; i < num_sectors; i++) {
+			rt_add_value(trace.sector + i, range_tree);
 		}
-		/* Skip over the extra data */
-		fread(&buf, 1, buf.pdu_len, stdin);
+
+		/* Skip over the extra data at end of trace buffer */
+		fread(&trace, 1, trace.pdu_len, stdin);
 	}
 	if (read_bytes != 0) {
 		perror("read");
