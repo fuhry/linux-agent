d1edf2a218de6a9832e4e14ba03de0fd57abcf6d
Jamie Simon <jsimon0@dattobackup.com>
2013-07-11 16:30:22 -0400

    actually fixed whitespace errors

--- rangetree/copyBlocks/copyblocks.c
+++ rangetree/copyBlocks/copyblocks.c
@@ -14,20 +14,20 @@ int copy_changes(int src_fd, int dest_fd, struct rb_root *root);
 
 int main(int argc, char* argv[])
 {
-	struct rb_root *range_tree = NULL;
-	struct blk_io_trace trace;
-	long total_read_bytes = 0;
-	int read_bytes = -1;
+    struct rb_root *range_tree = NULL;
+    struct blk_io_trace trace;
+    long total_read_bytes = 0;
+    int read_bytes = -1;
     int src_fd = -1;
     int dest_fd = -1;
-	unsigned int num_sectors;
-	unsigned int i;
-	
-	range_tree = malloc(sizeof(struct rb_root));
-	if (range_tree == NULL) {
-		perror("malloc");
-		return 1;
-	}
+    unsigned int num_sectors;
+    unsigned int i;
+    
+    range_tree = malloc(sizeof(struct rb_root));
+    if (range_tree == NULL) {
+        perror("malloc");
+        return 1;
+    }
     
     if (argc != 3) {
         printf("usage: %s file1 file2\n", argv[0]); 
@@ -47,42 +47,42 @@ int main(int argc, char* argv[])
         return 1;
     }
     
-	/* From stdin, read in a blk_io_trace struct. stdin should be the output
-	 * from blktrace or something similar */
-	while ((read_bytes = fread(&trace, sizeof(struct blk_io_trace), 1,
-					stdin) > 0)) {
-		total_read_bytes += read_bytes;
+    /* From stdin, read in a blk_io_trace struct. stdin should be the output
+     * from blktrace or something similar */
+    while ((read_bytes = fread(&trace, sizeof(struct blk_io_trace), 1,
+                    stdin) > 0)) {
+        total_read_bytes += read_bytes;
 
-		/* Sanity check, make sure the blk_io_trace struct has the right format */
-		if (trace.magic != (BLK_IO_TRACE_MAGIC | BLK_IO_TRACE_VERSION)) {
-			fprintf(stderr, "Error checking magic number\n");
-			fprintf(stderr, "  got magic: 0x%x\n", trace.magic);
-			fprintf(stderr, "  total_bytes: %ld\n",
-					total_read_bytes);
-			break;
-		}
+        /* Sanity check, make sure the blk_io_trace struct has the right format */
+        if (trace.magic != (BLK_IO_TRACE_MAGIC | BLK_IO_TRACE_VERSION)) {
+            fprintf(stderr, "Error checking magic number\n");
+            fprintf(stderr, "  got magic: 0x%x\n", trace.magic);
+            fprintf(stderr, "  total_bytes: %ld\n",
+                    total_read_bytes);
+            break;
+        }
         
-		/* Calculate the number of sectors in this trace */
-		num_sectors = trace.bytes / 512;
+        /* Calculate the number of sectors in this trace */
+        num_sectors = trace.bytes / 512;
         
-		/* Add each sector to the tree */
-		for (i = 0; i < num_sectors; i++) {
-			rt_add_value(trace.sector + i, range_tree);
-		}
+        /* Add each sector to the tree */
+        for (i = 0; i < num_sectors; i++) {
+            rt_add_value(trace.sector + i, range_tree);
+        }
         
-		/* Skip over the extra data at end of trace buffer */
-		fread(&trace, 1, trace.pdu_len, stdin);
-	}
+        /* Skip over the extra data at end of trace buffer */
+        fread(&trace, 1, trace.pdu_len, stdin);
+    }
     
-	if (total_read_bytes <= 0) {
-		perror("read");
+    if (total_read_bytes <= 0) {
+        perror("read");
     }
     
     if(copy_changes(src_fd, dest_fd, range_tree)) {
         printf("Error copying changes\n");
     }
     
-	rt_free_tree(range_tree);
+    rt_free_tree(range_tree);
     
-	return 0;
+    return 0;
 }
