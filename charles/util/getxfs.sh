3f770329b7f821db1dc923ea868decf9eecf223d
Charles Roydhouse <croydhouse@dattobackup.com>
2013-07-30 12:36:37 -0400

    partclone testing

--- charles/util/getxfs.sh
+++ charles/util/getxfs.sh
@@ -7,5 +7,7 @@ echo "Copying header files..."
 cp xfsprogs/include/*.h ../include
 echo "Copying dynamically linked library..."
 cp xfsprogs/libxfs/.libs/libxfs.so.0.0.0 ../lib/libxfs.so
+echo "Copying static linked library..."
+cp xfsprogs/libxfs/.libs/libxfs.a ../lib/
 echo "Cleaning up..."
 rm -Rf xfsprogs
