3db7c661c81d6b1f9979d7b43792d615c722f6c0
Nick Garvey <ngarvey@dattobackup.com>
2013-09-13 13:42:31 -0400

    Rework BlockDevice class

--- client/src/block_device/block_device.h
+++ client/src/block_device/block_device.h
@@ -6,22 +6,58 @@
 #include <string>
 
 namespace datto_linux_client {
+
 class BlockDevice : private boost::noncopyable {
+
  public:
-  virtual std::string block_path() const;
-  virtual uint32_t major() const;
-  virtual uint32_t minor() const;
-
-  virtual uint64_t SizeInBytes() const;
-
-  virtual ~BlockDevice();
- protected:
+  // Creates a BlockDevice from the given block_path
+  // An exception should be thrown (likely a BlockDeviceException) if the
+  // block_path doesn't exist or isn't the path for a block_device
   explicit BlockDevice(std::string block_path);
-  BlockDevice(uint32_t major, uint32_t minor);
+
+  // If a method is lowercase, then it is should be just an accessor and
+  // nothing more. See
+  // google-styleguide.googlecode.com/svn/trunk/cppguide.xml#Function_Names
+  std::string block_path() const;
+  uint32_t major() const;
+  uint32_t minor() const;
+
+  // If the block device needs to seek when reading/writing data.
+  // e.g. HDDs need to seek, SSDs do not.
+  bool DoesSeek() const;
+
+  // BLKGETSIZE64
+  uint64_t DeviceSizeBytes() const;
+  // Use BLKSSZGET not BLKBSZGET
+  uint64_t BlockSizeBytes() const;
+
+  // https://www.kernel.org/doc/Documentation/cgroups/blkio-controller.txt
+  // scalar is from 0 to 1, but hopefully not 0
+  void Throttle(double scalar);
+  double throttle_scalar() const;
+  void Unthrottle();
+
+  // Return a file descriptor for the block device
+  // Throw an exception if one is already open
+  int Open();
+
+  // Close the file descriptor returned
+  // Don't throw if one isn't open
+  void Close();
+
+  // Should close the file descriptor
+  ~BlockDevice();
+
+ private:
   std::string block_path_;
   uint32_t major_;
   uint32_t minor_;
+
+  double throttle_scalar_;
+
+  int file_descriptor_;
 };
+
 }
 
 #endif //  DATTO_CLIENT_BLOCK_DEVICE_BLOCK_DEVICE_H_
