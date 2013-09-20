36c19fcf5ef7db33447b77d0fc6b57740bb6dccc
Charles Roydhouse <croydhouse@dattobackup.com>
2013-09-20 08:53:33 -0400

    moved C XFS code into C++

--- to_integrate/cpp_parser/src/test.cpp
+++ to_integrate/cpp_parser/src/test.cpp
@@ -1,10 +1,19 @@
 // Test file
 #include "EXTFSPartition.h"
+#include "XFSPartition.h"
 using namespace datto_linux_client;
 
 int main(void) {
   EXTFSPartition extfs;
-  std::unique_ptr<const SectorSet> sectors = extfs.GetInUseSectors();
+  XFSPartition xfs;
+  std::unique_ptr<const SectorSet> sectors;
+  
+  sectors = extfs.GetInUseSectors();
+  for(SectorSet::iterator iter = sectors->begin(); iter != sectors->end(); ++iter) {
+    std::cout << *iter << std::endl;
+  }
+  
+  sectors = xfs.GetInUseSectors();
   for(SectorSet::iterator iter = sectors->begin(); iter != sectors->end(); ++iter) {
     std::cout << *iter << std::endl;
   }
