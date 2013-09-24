5de916c1fae57183c7c11e22ae0b3ab164203071
Nick Garvey <ngarvey@dattobackup.com>
2013-09-24 18:33:39 -0400

    Only track writes
    Putting this logic here means a lot of function calls, might want to
    inline or perform the filtering in the calling function if things get
    slow

--- client/block_trace/trace_handler.cc
+++ client/block_trace/trace_handler.cc
@@ -6,10 +6,12 @@ TraceHandler::TraceHandler(std::shared_ptr<UnsyncedSectorTracker> tracker)
     : tracker_(tracker) { }
 
 void TraceHandler::AddTrace(const struct blk_io_trace &trace_data) {
-  uint64_t sectors_written = trace_data.bytes / SECTOR_SIZE;
-  uint64_t sector = trace_data.sector;
-  SectorInterval interval(sector, sector + sectors_written);
-  (tracker_)->AddUnsyncedInterval(interval);
+  if (trace_data.action & BLK_TC_ACT(BLK_TC_WRITE)) {
+    uint64_t sectors_written = trace_data.bytes / SECTOR_SIZE;
+    uint64_t sector = trace_data.sector;
+    SectorInterval interval(sector, sector + sectors_written);
+    (tracker_)->AddUnsyncedInterval(interval);
+  }
 }
 
 }
