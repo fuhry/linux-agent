9e6aa26c58387a04941e972ec3283b558c81fe10
Nick Garvey <ngarvey@dattobackup.com>
2013-09-24 17:40:42 -0400

    Use bash to setup the loop device instead of C++
    Can't justify the time to use C++ instead of losetup
    Should likely abstract out the "doing things with a shell script"
    test logic

--- client/src/test/device_tracer_test.cc
+++ client/src/test/device_tracer_test.cc
@@ -2,6 +2,7 @@
 #include "block_trace/block_trace_exception.h"
 
 #include <memory>
+#include <fstream>
 #include <string.h>
 
 #include <errno.h>
@@ -25,6 +26,7 @@ using ::datto_linux_client::TraceHandler;
 class DeviceTracerTest : public ::testing::Test {
  public:
   static const uint64_t TEST_BLOCK_DEVICE_SIZE = 1 * 1024 * 1024 * 1024;
+  const std::string TEST_LOOP_SHARED_MEMORY = "/dev/shm/test_loop_path";
 
  protected:
   class DummyHandler : public TraceHandler {
@@ -37,86 +39,42 @@ class DeviceTracerTest : public ::testing::Test {
 
   DeviceTracerTest() {
     CreateEmptyLoopDevice(TEST_BLOCK_DEVICE_SIZE);
-    LOG(INFO) << loop_file_name;
-
-    SetLoopFD();
-    LOG(INFO) << loop_dev_fd;
+    LOG(INFO) << "Path is: " << loop_dev_path;
 
     handler = std::shared_ptr<TraceHandler>(new DummyHandler());
   }
 
-  virtual ~DeviceTracerTest() {
-    unlink(loop_file_name);
-    ClearLoopFD();
+  ~DeviceTracerTest() {
+    close(loop_dev_fd);
+    system(("losetup -d " + loop_dev_path).c_str());
+    unlink(TEST_LOOP_SHARED_MEMORY.c_str());
   }
 
-  char loop_file_name[L_tmpnam];
+  std::string loop_dev_path;
   int loop_dev_fd;
-  int loop_file_fd;
 
   std::shared_ptr<TraceHandler> handler;
 
  private:
-  void ClearLoopFD() {
-    ioctl(loop_dev_fd, LOOP_CLR_FD);
-  }
-
   void CreateEmptyLoopDevice(uint64_t size_bytes) {
-    tmpnam(loop_file_name);
+    int create_ret = system("./test/make_test_loop_device");
+    ASSERT_EQ(0, create_ret);
 
-    ASSERT_NE(nullptr, loop_file_name);
+    std::ifstream loop_path_stream(TEST_LOOP_SHARED_MEMORY);
+    std::getline(loop_path_stream, loop_dev_path);
+    loop_path_stream.close();
 
-    loop_file_fd = creat(loop_file_name, S_IRWXU);
-
-    ASSERT_NE(-1, loop_file_fd) << errno;
-
-    int trun_ret = ftruncate64(loop_file_fd, size_bytes);
-    ASSERT_EQ(0, trun_ret) << errno;
-  }
-
-  void SetLoopFD() {
-    struct loop_info64 loop_info;
-
-    // We don't need to do this on modern Linux's.. see
-    // https://lkml.org/lkml/2011/7/26/148
-    for (int i = 0; i < 100; ++i) {
-      std::string loop_path = "/dev/loop" + std::to_string(i);
-      loop_dev_fd = open(loop_path.c_str(), O_RDWR);
-
-      if (loop_dev_fd == -1) {
-        PLOG(ERROR) << "Error opening loop device " << i;
-        FAIL();
-      }
-
-      // If the ioctl succeeds, then the loop device already exists
-      if (ioctl(loop_dev_fd, LOOP_GET_STATUS64, &loop_info) >= 0) {
-        continue;
-      }
-
-      if (ioctl(loop_dev_fd, LOOP_SET_FD, loop_file_fd) < 0) {
-        PLOG(ERROR) << "Unable to assign file to loop device";
-        FAIL();
-      }
-
-      loop_info = {};
-      memcpy(loop_info.lo_file_name, loop_file_name,
-             strlen(loop_file_name) + 1);
-
-      if (ioctl(loop_dev_fd, LOOP_SET_STATUS64, &loop_info) < 0) {
-        PLOG(ERROR) << "Unable to set status for loop device";
-        ClearLoopFD();
-        FAIL();
-      }
-
-      break;
+    if ((loop_dev_fd = open(loop_dev_path.c_str(), O_WRONLY)) == -1) {
+      PLOG(ERROR) << "Unable to make test loop device. Verify everything is cleaned up with losetup";
+      unlink(TEST_LOOP_SHARED_MEMORY.c_str());
+      FAIL();
     }
   }
-
 };
 
 TEST_F(DeviceTracerTest, BadConstructor) {
   try {
-    DeviceTracer d("/dev/sda", handler);
+    DeviceTracer d(loop_dev_path, handler);
     sleep(10);
   } catch (const std::exception &e) {
     FAIL() << e.what();
