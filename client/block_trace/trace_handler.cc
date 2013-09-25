914c167253833b39f23aab99ab7526e39a8899c1
Nick Garvey <ngarvey@dattobackup.com>
2013-09-25 09:59:25 -0400

    Whitespace

--- client/block_trace/trace_handler.cc
+++ client/block_trace/trace_handler.cc
@@ -10,8 +10,10 @@ void TraceHandler::AddTrace(const struct blk_io_trace &trace_data) {
   if (trace_data.action & BLK_TC_ACT(BLK_TC_WRITE)) {
     VLOG(2) << "Adding trace with action 0x"
               << std::hex << trace_data.action << std::dec;
+
     uint64_t sectors_written = trace_data.bytes / SECTOR_SIZE;
     VLOG(2) << sectors_written << " sectors";
+
     uint64_t sector = trace_data.sector;
     SectorInterval interval(sector, sector + sectors_written);
     (tracker_)->AddUnsyncedInterval(interval);
