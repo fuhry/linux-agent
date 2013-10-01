923abed47896cf2c79c17f87b8a874ed165c7505
Nick Garvey <ngarvey@dattobackup.com>
2013-10-01 15:25:41 -0400

    Remove connect method
    Constructor should do this

--- client/remote_block_device/remote_block_device.h
+++ client/remote_block_device/remote_block_device.h
@@ -8,7 +8,6 @@ namespace datto_linux_client {
 
 class RemoteBlockDevice : BlockDevice {
  public:
-  virtual void Connect() = 0;
   virtual bool IsConnected() = 0;
   // Disconnect should not return an exception if already disconnected
   virtual void Disconnect() = 0;
