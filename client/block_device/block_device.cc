ddbbd4865db4223f3cef36e2cbc8bd6022b33a98
Nick Garvey <ngarvey@dattobackup.com>
2013-10-01 15:21:08 -0400

    Indentation and whitespace

--- client/block_device/block_device.cc
+++ client/block_device/block_device.cc
@@ -20,106 +20,92 @@
 
 namespace datto_linux_client {
 
-  BlockDevice::BlockDevice(std::string block_path) {
-    block_path_ = block_path;
-    Init();
+BlockDevice::BlockDevice(std::string block_path) {
+  block_path_ = block_path;
+  Init();
+}
+
+void BlockDevice::Init() {
+  struct stat statbuf;
+
+  //  Note:  using lstat() instead of stat() to cause symlinks to block devices to fail
+
+  if (lstat(block_path_.c_str(), &statbuf) < 0) {  // bail with exception if stat() fails
+    std::string err = std::string("Error: could not stat() ") + block_path_;
+    throw BlockDeviceException(err);
   }
 
-  void BlockDevice::Init() {
-
-    struct stat statbuf;
-
-    //  Note:  using lstat() instead of stat() to cause symlinks to block devices to fail
-
-    if (lstat(block_path_.c_str(), &statbuf) < 0) {  // bail with exception if stat() fails
-      std::string err = std::string("Error: could not stat() ") + block_path_;
-      throw BlockDeviceException(err);
-    }
-
-    if (! S_ISBLK(statbuf.st_mode) ) {  // bail with exception if not a block device
-      std::string err = std::string("Error: ") +
-          block_path_ +
-          std::string(" is not a block device");
-      throw BlockDeviceException(err);
-    }
-    
-    major_ = ::major(statbuf.st_rdev);  // Record major and minor numbers
-    minor_ = ::minor(statbuf.st_rdev);
-
-    int fd = 0;
-    fd = open(block_path_.c_str(),   //  Open readonly here, just for ioctl() purposes
-                  O_RDONLY | O_LARGEFILE);
-
-    if (fd < 0) {
-      std::string err = std::string("Error opening ") +
-          block_path_ +
-          std::string(" read-only for ioctl() calls\n");
-      throw BlockDeviceException(err);
-    }
-
-
-    ioctl(fd, BLKGETSIZE64, &device_size_bytes_);
-    ioctl(fd, BLKSSZGET, &block_size_bytes_);
-
-    close(fd);
-
-    throttle_scalar_ = 0.0;
-
-    file_descriptor_ = -1;
-
+  if (! S_ISBLK(statbuf.st_mode) ) {  // bail with exception if not a block device
+    std::string err = std::string("Error: ") +
+      block_path_ +
+      std::string(" is not a block device");
+    throw BlockDeviceException(err);
   }
 
-  void BlockDevice::Throttle(double scalar) {
-    throttle_scalar_ = scalar;
+  major_ = ::major(statbuf.st_rdev);  // Record major and minor numbers
+  minor_ = ::minor(statbuf.st_rdev);
+
+  int fd = 0;
+  fd = open(block_path_.c_str(),   //  Open readonly here, just for ioctl() purposes
+      O_RDONLY | O_LARGEFILE);
+
+  if (fd < 0) {
+    std::string err = std::string("Error opening ") +
+      block_path_ +
+      std::string(" read-only for ioctl() calls\n");
+    throw BlockDeviceException(err);
   }
 
-  void BlockDevice::Unthrottle() {
-    throttle_scalar_ = 0.0;;
+  ioctl(fd, BLKGETSIZE64, &device_size_bytes_);
+  ioctl(fd, BLKSSZGET, &block_size_bytes_);
+
+  close(fd);
+
+  throttle_scalar_ = 0.0;
+  file_descriptor_ = -1;
+}
+
+void BlockDevice::Throttle(double scalar) {
+  throttle_scalar_ = scalar;
+}
+
+void BlockDevice::Unthrottle() {
+  throttle_scalar_ = 0.0;;
+}
+
+int BlockDevice::Open() {
+  if (file_descriptor_ != -1) {
+    std::string err = "Error: block device " + block_path_ + 
+      " already open";
+    throw BlockDeviceException(err);
   }
 
-  int BlockDevice::Open() {
+  int fd = open(block_path_.c_str(),
+      O_RDWR | O_LARGEFILE);
 
-    if (file_descriptor_ != -1) {
-      std::string err = "Error: block device " + block_path_ + 
-        " already open";
-      throw BlockDeviceException(err);
-    }
-
-    int fd = open(block_path_.c_str(),
-                  O_RDWR | O_LARGEFILE);
-
-    if (fd < 0) {
-      char * error_chars = strerror(errno);
-          std::string err = std::string("Error opening ") +
-          block_path_ +
-          std::string("; error: ") +
-          std::string(error_chars);
-      throw BlockDeviceException(err);
-    }
-
-    file_descriptor_ = fd;
-    return fd;
-
+  if (fd < 0) {
+    char * error_chars = strerror(errno);
+    std::string err = std::string("Error opening ") +
+      block_path_ +
+      std::string("; error: ") +
+      std::string(error_chars);
+    throw BlockDeviceException(err);
   }
 
-  void BlockDevice::Close() {
+  file_descriptor_ = fd;
+  return fd;
+}
 
-    if (file_descriptor_ > -1) {
-      close(file_descriptor_);
-    }
-    file_descriptor_ = -1;
+void BlockDevice::Close() {
+  if (file_descriptor_ > -1) {
+    close(file_descriptor_);
   }
+  file_descriptor_ = -1;
+}
 
-  BlockDevice::~BlockDevice() {
+BlockDevice::~BlockDevice() {
+  Close();
+  Unthrottle();
+}
 
-    Close();
-    Unthrottle();
-
-  }
-
-
- }  //namespace   
-
-
-
-    
+}  //namespace   
