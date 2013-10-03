2133a5ba46ef1ead335e62fce9cc32455597d3bd
Nick Garvey <ngarvey@dattobackup.com>
2013-10-03 14:32:31 -0400

    Rework block_device_test with test framework

--- client/test/block_device_test.cc
+++ client/test/block_device_test.cc
@@ -1,58 +1,76 @@
 // TODO: Integrate this with Google Test
 #include <block_device/block_device.h>
 
-#include <iostream>
+#include <fcntl.h>
+#include <gtest/gtest.h>
+#include <memory>
+#include <unistd.h>
 
-int main(int argc, char **argv) {
+#include "test/loop_device.h"
 
-  using std::cerr;
-  using std::cout;
-  using std::endl;
+namespace {
+using ::datto_linux_client::BlockDevice;
+using ::datto_linux_client_test::LoopDevice;
 
-  using datto_linux_client::BlockDeviceException;
-  using datto_linux_client::BlockDevice;
-
-  if (argc < 2) {
-    cerr << "Error: must provide one argument (a block device path name)\n";
-    exit(1);
+class BlockDeviceTest : public ::testing::Test {
+ protected:
+  BlockDeviceTest() {
+    loop_device = std::unique_ptr<LoopDevice>(new LoopDevice());
   }
+  std::unique_ptr<LoopDevice> loop_device;
+};
 
-  std::string block_dev = argv[1];
-
-  BlockDevice * bd1;
-  try {
-    bd1 = new BlockDevice(block_dev);
-  }
-  catch (BlockDeviceException &e) {
-    cerr << "Caught BlockDeviceException while creating new BlockDevice object:\n";
-    cerr << e.what();
-    cerr << "\n";
-    exit(1);
-  }
-
-  cout << "Block Path Name: " << bd1->block_path() << endl;
-  cout << "\tMajor number is " << bd1->major() << endl;
-  cout << "\tMinor number is " << bd1->minor() << endl;
-  cout << "\tDevice Size is " << bd1->DeviceSizeBytes() << endl;
-  cout << "\tBlock Size is " << bd1->BlockSizeBytes() << endl;
-
-  int fd;
-  try {
-    fd = bd1->Open();
-  }
-  catch (BlockDeviceException &e) {
-    cerr << "Caught BlockDeviceException while creating opening BlockDevice object:\n";
-    cerr << e.what();
-    cerr << "\n";
-    exit(1);
-  }
-
-  cout << "\tDevice opened, file descriptor is " << fd << endl;
-
-  exit(0);
-
+TEST_F(BlockDeviceTest, Constructor) {
+  BlockDevice bd(loop_device->path());
 }
 
+TEST(BlockDeviceTestNoFixture, BadConstructor) {
+  bool did_throw = false;
+  try {
+    BlockDevice bd("Not a path");
+  } catch(const std::runtime_error &e) {
+    // Good, this should have thrown an exception
+    did_throw = true;
+  }
+  EXPECT_TRUE(did_throw);
+}
 
+TEST_F(BlockDeviceTest, Path) {
+  BlockDevice bd(loop_device->path());
+  EXPECT_EQ(loop_device->path(), bd.block_path());
+}
 
+TEST_F(BlockDeviceTest, BlockSizeBytes) {
+  BlockDevice bd(loop_device->path());
+  EXPECT_EQ(loop_device->block_size(), bd.BlockSizeBytes());
+}
 
+TEST_F(BlockDeviceTest, Open) {
+  BlockDevice bd(loop_device->path());
+  int bd_fd = bd.Open();
+  EXPECT_GT(bd_fd, 2);
+}
+
+TEST_F(BlockDeviceTest, Major) {
+  BlockDevice bd(loop_device->path());
+  int major = bd.major();
+  // 7 is the loop device major
+  EXPECT_EQ(7, major);
+}
+
+TEST_F(BlockDeviceTest, DestructorCloses) {
+  int bd_fd = -1;
+
+  {
+    BlockDevice bd(loop_device->path());
+    bd_fd = bd.Open();
+  } // destructor called here
+
+  int fcntl_ret = fcntl(bd_fd, F_GETFD);
+  int error = errno;
+
+  EXPECT_EQ(EBADF, error);
+  EXPECT_EQ(-1, fcntl_ret);
+}
+
+} // namespace
