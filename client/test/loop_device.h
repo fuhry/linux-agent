619b94fbf36be5440da7529ef3eda946cbda2737
Nick Garvey <ngarvey@dattobackup.com>
2013-10-03 14:08:49 -0400

    Remove unused global

--- client/test/loop_device.h
+++ client/test/loop_device.h
@@ -7,7 +7,6 @@ namespace datto_linux_client_test {
 // TODO: This class assumes that there is a /dev/shm and /tmp is the
 // temporary directory. These assumptions should be made more explicit
 // or removed.
-static const uint64_t TEST_BLOCK_DEVICE_SIZE = 1 * 1024 * 1024 * 1024;
 static const char TEST_LOOP_SHARED_MEMORY[] = "/dev/shm/test_loop_path";
 
 class LoopDevice {
