dbc8dcd198576a5777ffded36b5f87cb854426aa
Nick Garvey <ngarvey@dattobackup.com>
2013-10-03 13:43:38 -0400

    Improve precondition comment

--- client/block_device/mountable_block_device.h
+++ client/block_device/mountable_block_device.h
@@ -22,8 +22,9 @@ class MountableBlockDevice : public BlockDevice {
 
   // These should only be overwritten if the underlying filesystem
   // doesn't support the FIFREEZE ioctl
-  // Note that the MountableBlockDevice must not have an open
-  // file descriptor for the mount point.
+  //
+  // Before calling these the MountableBlockDevice must not have an open
+  // file descriptor for the mount point (i.e., call CloseMount() first)
   virtual void Freeze();
   virtual void Thaw();
 
