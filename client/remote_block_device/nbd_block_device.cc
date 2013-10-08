35365bff8af678f46860d59a9f02a07d7dd998ee
Nick Garvey <ngarvey@dattobackup.com>
2013-10-08 16:31:49 -0400

    Remove old comment and old includes

--- client/remote_block_device/nbd_block_device.cc
+++ client/remote_block_device/nbd_block_device.cc
@@ -1,19 +1,11 @@
 #include "remote_block_device/nbd_block_device.h"
 #include "remote_block_device/remote_block_device_exception.h"
 
-#include <fcntl.h>
 #include <glog/logging.h>
-#include <sstream>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <sys/wait.h>
 #include <unistd.h>
 
 namespace datto_linux_client {
 
-// TODO: If this ends up making it to prod, we should stop using system
-// and start using exec(). As I *think* this is temporary, don't worry about
-// this yet.
 NbdBlockDevice::NbdBlockDevice(std::string remote_host, uint16_t remote_port)
     : RemoteBlockDevice() {
   nbd_client_ = std::unique_ptr<NbdClient>(new NbdClient(remote_host,
