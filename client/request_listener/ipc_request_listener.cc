3ce4b5b28c8ab145182e8802957d680103568502
Nick Garvey <ngarvey@dattobackup.com>
2013-10-27 15:45:22 -0400

    Correct functionality errors discovered from tests

--- client/request_listener/ipc_request_listener.cc
+++ client/request_listener/ipc_request_listener.cc
@@ -1,18 +1,22 @@
 #include "request_listener/ipc_request_listener.h"
 
 #include <glog/logging.h>
+#include <chrono>
+#include <thread>
 #include <vector>
 
+#include <arpa/inet.h>
+#include <poll.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <sys/un.h>
-#include <arpa/inet.h>
 
 #include "request_listener/request_listener_exception.h"
 #include "request_listener/socket_reply_channel.h"
 
 #include "request.pb.h"
 
+// TODO freeaddrinfo
 namespace {
 
 using ::datto_linux_client::RequestListenerException;
@@ -21,10 +25,9 @@ using ::datto_linux_client::Request;
 const int SOCKET_BACKLOG = 5;
 
 int open_socket(std::string ipc_socket_path) {
-  int sock_fd;
   const char *ipc_path_cstr = ipc_socket_path.c_str();
 
-  sock_fd = socket(AF_UNIX, SOCK_STREAM, 0);
+  int sock_fd = socket(AF_UNIX, SOCK_STREAM, 0);
 
   if (sock_fd < 0) { 
     PLOG(ERROR) << "Unable to open socket";
@@ -56,7 +59,23 @@ int open_socket(std::string ipc_socket_path) {
   return sock_fd;
 }
 
-// TODO Timeout
+bool connection_is_ready(int socket_fd, int timeout_millis) {
+  struct pollfd pfd;
+  pfd.fd = socket_fd;
+  pfd.events = POLLIN;
+  switch (poll(&pfd, 1, timeout_millis)) {
+    case -1:
+      PLOG(ERROR) << "Unable to poll file descriptor";
+      throw RequestListenerException("Poll failure");
+    case 0:
+      return false;
+    case 1:
+      return true;
+    default:
+      throw RequestListenerException("Unexpected branch");
+  }
+}
+
 int get_new_connection(int socket_fd) {
   int connection_fd = accept(socket_fd, NULL, NULL);
   if (connection_fd < 0) {
@@ -81,9 +100,10 @@ Request read_protobuf_request(int connection_fd) {
   }
 
   message_length = ntohl(message_length);
+  LOG(INFO) << "Getting request of size: " << message_length;
 
   // Sanity check, shouldn't be larger than 1MB
-  if (message_length < 1 * 1024 * 1024) {
+  if (message_length > 1 * 1024 * 1024) {
     LOG(ERROR) << "Message length: " << message_length;
     throw RequestListenerException("Message was too big");
   }
@@ -94,7 +114,7 @@ Request read_protobuf_request(int connection_fd) {
   bytes_read = recv(connection_fd, &message_buf[0], message_length, 
                     MSG_WAITALL);
 
-  if (bytes_read != message_length) {
+  if (bytes_read != (int32_t)message_length) {
     PLOG(ERROR) << "Bytes read: " << bytes_read;
     throw RequestListenerException("Error reading request");
   }
@@ -117,29 +137,44 @@ IpcRequestListener::IpcRequestListener(
 
   socket_fd_ = open_socket(ipc_socket_path);
 
+  std::atomic<bool> started(false);
+
   listen_thread_ = std::thread([&]() {
     while (!do_stop_) {
       try {
-        int connection_fd = get_new_connection(socket_fd_);
-        auto request = read_protobuf_request(connection_fd);
-        
-        std::shared_ptr<ReplyChannel> reply_channel(
-            new SocketReplyChannel(connection_fd));
+        if (connection_is_ready(socket_fd_, 500)) {
+          if (do_stop_) {
+            break;
+          }
+          int connection_fd = get_new_connection(socket_fd_);
+          auto request = read_protobuf_request(connection_fd);
+          
+          std::shared_ptr<ReplyChannel> reply_channel(
+              new SocketReplyChannel(connection_fd));
 
-        request_handler_->Handle(request, reply_channel);
+          request_handler_->Handle(request, reply_channel);
+        }
       } catch (const std::runtime_error &e) {
-        LOG(ERROR) << e.what();
+        LOG(ERROR) << "Exception during connection loop: " << e.what();
       }
+      started = true;
     }
   });
+
+  while (!started) {
+    std::this_thread::yield();
+  }
 }
 
 void IpcRequestListener::Stop() {
   do_stop_ = true;
+  // TODO Make sure this doesn't interrupt any ReplyChannel communication,
+  // it should only interfere with creating new connections
+  close(socket_fd_);
 }
 
 IpcRequestListener::~IpcRequestListener() {
-  close(socket_fd_);
+  LOG(INFO) << "Tearing down IpcRequestListener";
   Stop();
   listen_thread_.join();
 }
--- client/request_listener/socket_reply_channel.cc
+++ client/request_listener/socket_reply_channel.cc
@@ -1,4 +1,5 @@
 #include "request_listener/socket_reply_channel.h"
+#include <arpa/inet.h>
 
 #include <glog/logging.h>
 
@@ -8,6 +9,14 @@ SocketReplyChannel::SocketReplyChannel(int connection_fd)
     : connection_fd_(connection_fd) { }
 
 void SocketReplyChannel::SendReply(const Reply &reply) {
+  uint32_t reply_size = reply.ByteSize();
+  reply_size = htonl(reply_size);
+
+  if (write(connection_fd_, &reply_size, sizeof(reply_size)) == -1) {
+    PLOG(ERROR) << "Error writing to socket fd";
+    throw std::runtime_error("Error writing to socket fd");
+  }
+
   if (!reply.SerializeToFileDescriptor(connection_fd_)) {
     PLOG(ERROR) << "Error on SerializeToFileDescriptor";
     throw RequestListenerException("Error sending over reply channel");
