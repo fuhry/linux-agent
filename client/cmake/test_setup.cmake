f370757a0086d94095e8c8bf7e5bd16857a1cd0e
Nick Garvey <ngarvey@dattobackup.com>
2014-01-15 10:06:48 -0500

    Switch to GMock
    Also speeds up the entire build because no more svn

--- client/cmake/test_setup.cmake
+++ client/cmake/test_setup.cmake
@@ -3,6 +3,12 @@
 
 enable_testing()
 
+# Build GTest
+set(GTEST_DIR "/usr/src/gtest")
+include_directories(${GTEST_DIR})
+include_directories(${GTEST_DIR}/include)
+add_library(gtest ${GTEST_DIR}/src/gtest-all.cc)
+
 add_custom_target(check)
 # usage: add_unit_test(test_name test_sources...)
 # Note that add_unit_test automatically finds test/${test_name}.cc
@@ -11,9 +17,11 @@ macro(add_unit_test test_name)
     add_executable(${test_name} EXCLUDE_FROM_ALL test/${test_name}.cc ${ARGN})
     add_dependencies(${test_name} gtest)
 
+    include_directories(${CMAKE_CURRENT_BINARY_DIR})
     target_link_libraries(${test_name} glog)
-    target_link_libraries(${test_name} ${GTEST_BINARIES}/libgtest.a)
-    target_link_libraries(${test_name} ${GTEST_BINARIES}/libgtest_main.a)
+    target_link_libraries(${test_name} gtest)
+    target_link_libraries(${test_name} gmock)
+    target_link_libraries(${test_name} gmock_main)
 
     # TODO set_tests_properties should do this so we don't need to do the
     # bash -c hack here, but can't seem to get it to work
@@ -30,27 +38,3 @@ macro(add_unit_test test_name)
     add_dependencies(check ctest_${test_name})
 endmacro()
 
-### http://stackoverflow.com/a/9695234/965648
-include(ExternalProject)
-# Add gtest
-externalproject_add(
-    gtest
-    SVN_REPOSITORY http://googletest.googlecode.com/svn/trunk/
-    SVN_REVISION -r 660
-    TIMEOUT 10
-    PREFIX "${CMAKE_CURRENT_BINARY_DIR}/gtest"
-    # Disable install step
-    INSTALL_COMMAND ""
-    # Wrap download, configure and build steps in a script to log output
-    LOG_DOWNLOAD ON
-    LOG_CONFIGURE ON
-    LOG_BUILD ON)
-
-externalproject_get_property(gtest source_dir)
-externalproject_get_property(gtest binary_dir)
-set(GTEST_INCLUDES "${source_dir}/include")
-set(GTEST_BINARIES "${binary_dir}")
-set(GTEST_LIBRARIES "${binary_dir}/libgtest.a ${binary_dir}/libgtest-main.a")
-
-
-include_directories("${GTEST_INCLUDES}")
