0ede41feff5627ee0f3a69f84f8386ebf8f89d90
Nick Garvey <ngarvey@dattobackup.com>
2014-02-13 12:45:48 -0500

    Move fetch_testing_tools script into CMake

--- client/cmake/test_setup.cmake
+++ client/cmake/test_setup.cmake
@@ -3,22 +3,27 @@
 
 enable_testing()
 
-# Build GTest and GMock, fetch_testing_tools needs to have been run first
-set(GMOCK_DIR "${CMAKE_CURRENT_SOURCE_DIR}/gmock/")
-set(GTEST_DIR "${CMAKE_CURRENT_SOURCE_DIR}/gmock/gtest/")
-if (EXISTS ${GMOCK_DIR} AND EXISTS ${GTEST_DIR})
-    include_directories(${GTEST_DIR})
-    include_directories(${GTEST_DIR}/include)
-    include_directories(${GMOCK_DIR})
-    include_directories(${GMOCK_DIR}/include)
-    add_library(gtest ${GTEST_DIR}/src/gtest-all.cc)
-    add_library(gmock_main ${GMOCK_DIR}/src/gmock-all.cc
-                           ${GMOCK_DIR}/src/gmock_main.cc)
-else()
-    add_library(FATAL_ERROR "gmock can't be found, run ./fetch_testing_tools")
-endif()
+set(GMOCK_DIR "${CMAKE_CURRENT_BINARY_DIR}/gmock/")
+set(GTEST_DIR "${CMAKE_CURRENT_BINARY_DIR}/gmock/gtest/")
+# On 12.04: We need r403, but r433 breaks the build, so use r432
+add_custom_command(OUTPUT ${GMOCK_DIR}/src/gmock-all.cc
+                          ${GMOCK_DIR}/src/gmock_main.cc
+                          ${GTEST_DIR}/src/gtest-all.cc
+                   COMMAND svn checkout -r 432
+                           http://googlemock.googlecode.com/svn/trunk gmock)
 
+include_directories(${GTEST_DIR})
+include_directories(${GTEST_DIR}/include)
+include_directories(${GMOCK_DIR})
+include_directories(${GMOCK_DIR}/include)
+add_library(gtest ${GTEST_DIR}/src/gtest-all.cc)
+add_library(gmock_main ${GMOCK_DIR}/src/gmock-all.cc
+                       ${GMOCK_DIR}/src/gmock_main.cc)
+add_dependencies(gtest gmock_main)
+add_dependencies(gmock_main testing_tools)
 add_custom_target(check)
+
+
 # usage: add_unit_test(test_name test_sources...)
 # Note that add_unit_test automatically finds test/${test_name}.cc
 # find_library(Protobuf) must have been called before calling this
@@ -29,9 +34,6 @@ macro(add_unit_test test_name)
     include_directories(${CMAKE_CURRENT_BINARY_DIR})
     target_link_libraries(${test_name} glog)
     target_link_libraries(${test_name} gtest)
-    if (NOT EXISTS ${GMOCK_DIR})
-        target_link_libraries(${test_name} gmock)
-    endif()
     target_link_libraries(${test_name} gmock_main)
 
     # TODO set_tests_properties should do this so we don't need to do the
