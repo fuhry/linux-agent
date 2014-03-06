1da071a29605d3dcbcc469a62e63d1714962d538
Nick Garvey <ngarvey@dattobackup.com>
2014-03-06 11:11:36 -0500

    Suppress gcc warning
    http://stackoverflow.com/a/9225648/965648
    http://stackoverflow.com/a/8132440/965648

--- backup_status_tracker/printing_sync_count_handler.cc
+++ backup_status_tracker/printing_sync_count_handler.cc
@@ -1,15 +1,17 @@
 #include "backup_status_tracker/printing_sync_count_handler.h"
 
+#define __STDC_FORMAT_MACROS
 #include <stdio.h>
+#include <inttypes.h>
 
 namespace datto_linux_client {
 
 PrintingSyncCountHandler::PrintingSyncCountHandler(uint64_t bytes_total) {
-  printf("%lu total bytes\n", bytes_total);
+  printf("%" PRIu64 " total bytes\n", bytes_total);
 }
 
 void PrintingSyncCountHandler::UpdateSyncedCount(uint64_t num_synced_a) {
-  printf("%lu synced bytes\n", num_synced_a);
+  printf("%" PRIu64 " synced bytes\n", num_synced_a);
 }
 
 } // datto_linux_client
