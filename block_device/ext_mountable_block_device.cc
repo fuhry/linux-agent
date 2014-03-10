d78e5d69fc6b8dd1b0effbb60cb925b40588d431
Nick Garvey <ngarvey@dattobackup.com>
2014-03-10 16:30:58 -0400

    Add FreezeHelper logic to ExtMountableBlockDevice

--- block_device/ext_mountable_block_device.cc
+++ block_device/ext_mountable_block_device.cc
@@ -5,26 +5,58 @@
 
 #include <glog/logging.h>
 
-#include "block_device/ext_file_system.h"
-#include "block_device/ext_error_table-inl.h"
 #include "block_device/block_device_exception.h"
-#include "unsynced_sector_manager/sector_interval.h"
+#include "block_device/ext_error_table-inl.h"
+#include "block_device/ext_file_system.h"
 #include "block_device/tools.h"
+#include "freeze_helper/freeze_helper.h"
+#include "unsynced_sector_manager/sector_interval.h"
 
 namespace datto_linux_client {
 
-ExtMountableBlockDevice::ExtMountableBlockDevice(std::string a_path, bool is_ext2)
+ExtMountableBlockDevice::ExtMountableBlockDevice(std::string a_path,
+                                                 bool is_ext2)
     : MountableBlockDevice(a_path), is_ext2_(is_ext2) { }
 
 std::shared_ptr<const SectorSet> ExtMountableBlockDevice::GetInUseSectors() {
   std::shared_ptr<SectorSet> sectors(new SectorSet());
   ExtErrorTable error_table;
-  ExtFileSystem ext_fs(BlockDevice::path(), error_table);
 
-  uint32_t blocks_per_group = ext_fs.super()->s_blocks_per_group;
-  uint32_t block_size = 0x400 << ext_fs.super()->s_log_block_size;
-  uint64_t sectors_per_block = block_size / 512;
-  uint32_t bitmap_size = my_roundup(blocks_per_group, 8) / 8;
+  FreezeHelper freeze_helper(*this, 2000);
+
+  int freeze_attempts = 0;
+
+  std::unique_ptr<ExtFileSystem> ext_fs;
+  do {
+    freeze_attempts++;
+    if (freeze_attempts > 10) {
+      throw BlockDeviceException("Unable to read off of disk quickly enough");
+    }
+    freeze_helper.BeginRequiredFreezeBlock();
+    ext_fs = std::unique_ptr<ExtFileSystem>(
+        new ExtFileSystem(BlockDevice::path(), error_table));
+  } while (!freeze_helper.EndRequiredFreezeBlock());
+
+  uint32_t blocks_per_group;
+  uint32_t block_size;
+  uint64_t sectors_per_block;
+  uint32_t bitmap_size;
+  // TODO
+  uint64_t group_desc_count;
+
+  freeze_attempts = 0;
+  do {
+    freeze_attempts++;
+    if (freeze_attempts > 10) {
+      throw BlockDeviceException("Unable to read off of disk quickly enough");
+    }
+    freeze_helper.BeginRequiredFreezeBlock();
+    blocks_per_group = ext_fs->super()->s_blocks_per_group;
+    block_size = 0x400 << ext_fs->super()->s_log_block_size;
+    sectors_per_block = block_size / 512;
+    bitmap_size = my_roundup(blocks_per_group, 8) / 8;
+    group_desc_count = ext_fs->group_desc_count();
+  } while (!freeze_helper.EndRequiredFreezeBlock());
 
   std::unique_ptr<char[]> block_bitmap(new char[bitmap_size]);
 
@@ -33,45 +65,62 @@ std::shared_ptr<const SectorSet> ExtMountableBlockDevice::GetInUseSectors() {
   sectors->insert(SectorInterval(0, 2));
 
   // Iterates over every group in the file system
-  for (uint64_t i = 0; i < ext_fs.group_desc_count(); ++i) {
+  for (uint64_t i = 0; i < group_desc_count; ++i) {
+    off_t cur_group_block_offset;
     DLOG(INFO) << "Checking ext group #" << i;
+    freeze_attempts = 0;
+    do {
+      freeze_attempts++;
+      if (freeze_attempts > 10) {
+        throw BlockDeviceException(
+            "Unable to read off of disk quickly enough");
+      }
+      freeze_helper.BeginRequiredFreezeBlock();
 
-    // Calculate the block offset for the current group
-    off_t cur_group_block_offset = ext_fs.super()->s_first_data_block +
+      // Calculate the block offset for the current group
+      cur_group_block_offset = ext_fs->super()->s_first_data_block +
                                    (i * blocks_per_group);
 
-    // Load the block bitmap for this group
-    ext2fs_get_block_bitmap_range(ext_fs.block_map(),
-                                  cur_group_block_offset,
-                                  blocks_per_group,
-                                  block_bitmap.get());
+      // Load the block bitmap for this group
+      ext2fs_get_block_bitmap_range(ext_fs->block_map(),
+                                    cur_group_block_offset,
+                                    blocks_per_group,
+                                    block_bitmap.get());
+    } while (!freeze_helper.EndRequiredFreezeBlock());
+
+    // TODO
+    uint64_t blocks_count = ext_fs->super()->s_blocks_count;
 
     // Iterate over every block assigned to this group
     // Break if we have reached the end of this group (locks_per_group)
     // or the end of the file system (s_blocks_count)
     for (uint64_t j = 0;
          (j < blocks_per_group &&
-            (j + cur_group_block_offset < ext_fs.super()->s_blocks_count));
+            (j + cur_group_block_offset < blocks_count));
          ++j) {
+      freeze_attempts = 0;
+      do {
+        freeze_attempts++;
+        if (freeze_attempts > 10) {
+          throw BlockDeviceException(
+              "Unable to read off of disk quickly enough");
+        }
+        freeze_helper.BeginRequiredFreezeBlock();
+        // If the bit is set then the block is allocated.
+        if (ext2fs_test_bit(j, block_bitmap.get())) {
+          off_t cur_block_offset = cur_group_block_offset + j;
 
-      this->Freeze();
+          // Calculate the sector for the current block
+          uint64_t sector_location = sectors_per_block * cur_block_offset;
 
-      // If the bit is set then the block is allocated.
-      if (ext2fs_test_bit(j, block_bitmap.get())) {
-        off_t cur_block_offset = cur_group_block_offset + j;
-
-        // Calculate the sector for the current block
-        uint64_t sector_location = sectors_per_block * cur_block_offset;
-
-        // Insert an interval starting at the sector for the current block
-        // and ending at the last sector for the current block
-        // Remember that the SectorIntervals are [x, y) where x is 
-        // inclusive and y is exclusive.
-        sectors->insert(SectorInterval(sector_location,
-                                       sector_location + sectors_per_block));
-      }
-
-      this->Thaw();
+          // Insert an interval starting at the sector for the current block
+          // and ending at the last sector for the current block
+          // Remember that the SectorIntervals are [x, y) where x is 
+          // inclusive and y is exclusive.
+          sectors->insert(SectorInterval(sector_location,
+                                         sector_location + sectors_per_block));
+        }
+      } while (!freeze_helper.EndRequiredFreezeBlock());
     }
   }
 
