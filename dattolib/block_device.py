2d947dc3ef4f7014d6662d163fd77bdce61a66f2
Nick Garvey <ngarvey@dattobackup.com>
2014-04-06 19:22:48 -0400

    Add get_mount_point support to dattolib

--- dattolib/block_device.py
+++ dattolib/block_device.py
@@ -2,6 +2,8 @@ import ctypes
 import ctypes.util
 from fcntl import ioctl
 import struct
+import os
+import os.path
 
 
 # blkid setup and related functions
@@ -54,6 +56,54 @@ def get_block_size(path):
         return struct.unpack('L', buf)[0]
 
 
+# libc setup and related functions
+
+class MntEnt(ctypes.Structure):
+    """ Struct defined in mntent.h """
+    _fields_ = [
+        ("mnt_fsname", ctypes.c_char_p),
+        ("mnt_dir", ctypes.c_char_p),
+        ("mnt_type", ctypes.c_char_p),
+        ("mnt_opts", ctypes.c_char_p),
+        ("mnt_freq", ctypes.c_int),
+        ("mnt_passno", ctypes.c_int)
+    ]
+
+
+_libc_path = ctypes.util.find_library('c')
+if not _libc_path:
+    raise EnvironmentError("libc library not found (!!)")
+_libc = ctypes.CDLL(_libc_path, use_errno=True)
+_libc.getmntent.restype = ctypes.POINTER(MntEnt)
+
+
+def get_mount_point(path):
+    with open("/proc/mounts") as mounts:
+        # See "man 2 mntent" to understand this section
+        # and the variable names
+        file_p = _libc.fdopen(mounts.fileno(), "r")
+        while True:
+            # This is a struct containing pointers to static memory.
+            # As such, we shouldn't copy the mntent structure as the values
+            # it points to will be overwitten in the getmntent call
+            mntent = _libc.getmntent(file_p)
+            if not mntent:
+                break
+
+            # Make sure the device starts with / or we know it's some
+            # virtual thing we don't want to look at
+            if mntent.contents.mnt_fsname[0] != '/':
+                continue
+            fsname = mntent.contents.mnt_fsname
+
+            if os.path.islink(fsname):
+                fsname_dir = os.path.dirname(fsname)
+                fsname = os.path.join(fsname_dir, os.readlink(fsname))
+            if os.path.abspath(path) == os.path.abspath(fsname):
+                return mntent.contents.mnt_dir
+    return None
+
+
 if __name__ == "__main__":
     import sys
     path = sys.argv[1] if len(sys.argv) > 1 else "/dev/sda1"
@@ -61,3 +111,4 @@ if __name__ == "__main__":
     print "uuid:", get_uuid(path)
     print "size:", get_size(path)
     print "block size:", get_block_size(path)
+    print "mount point:", get_mount_point(path)
